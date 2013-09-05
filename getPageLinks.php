59f32cc97e08d3761c35323c0dd87e173db63b83
bladebo <bladebo+git@gmail.com>
2013-09-05 16:51:59 +0200

    Update getPageLinks.php

--- getPageLinks.php
+++ getPageLinks.php
@@ -3,6 +3,13 @@
 $url = 'http://yourUrl'; // URI with a page of folder links
 $file = file_get_contents($url);
 
+$resultsFile = array();
+preg_match_all('#<a.+href="(.*[.].{3})">.+</a>#', $file, $resultsFile);
+//ECHO 'FILES IN '.$url. '<br />';
+foreach($resultsFile[1] as $dataFile){
+    echo $url.$dataFile . '<br />';
+}
+
 $results = array();
 preg_match_all('#<a.+href="(.*)">.+</a>#', $file, $results);   
 //ECHO 'FOLDER'. '<br />';
@@ -16,7 +23,7 @@ foreach($results[1] as $data)
 	$file2 = file_get_contents($FolderUrl);
 	$results2 = array();
 	preg_match_all('#<a.+href="(.*[.].{3})">.+</a>#', $file2, $results2);
-	//ECHO 'FILE IN FOLDER '.$FolderUrl. '<br />';
+	//ECHO 'FILES IN FOLDER '.$FolderUrl. '<br />';
 	foreach($results2[1] as $data2)
     	echo $FolderUrl.$data2 . '<br />';
 }
